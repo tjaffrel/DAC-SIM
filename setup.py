d12ef23aa8265c101ec587f6d8c1e19f5add5147
hspark1212 <phs68660888@gmail.com>
2024-12-11 09:09:39 +0000

    add models in setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,11 @@ with open("requirements.txt", "r") as f:
 
 setup(
     name="dac-sim",
-    version="0.0.2",
+    version="0.0.3",
     packages=find_packages(),
     install_requires=install_requires,
     include_package_data=True,
+    package_data={"dac_sim": ["models/*"]},
     entry_points={
         "console_scripts": [
             "dac-sim = dac_sim.cli:cli",
