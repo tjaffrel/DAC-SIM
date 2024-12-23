1fcc4e969451f542caa07b9db51fa3a8fd705201
tjaffrel <theo.jaf@gmail.com>
2024-12-22 23:33:07 -0800

    cleaned submission script

--- submission_script.py
+++ submission_script.py
@@ -7,8 +7,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Load the structure and build the gas
-structure = read("/home/theoj/project/fennol_atomate2/DFT_optimized_structures_cof999/COF999_pristine_2branches/pristine_g1.cif")
-# structure = read("examples/mg-mof-74.cif")
+structure = read("examples/mg-mof-74.cif")
 gas = molecule("CO2")
 
 
@@ -25,7 +24,6 @@ for temp in temperatures:
         temperature=temp,
         trajectory=None,
         logfile=None,
-        #model_path="/home/theoj/project/fennol_atomate2/crate_trained.fnx",
         model_path="/home/theoj/programs/DAC-SIM/dac_sim/models/mace-dac-1.model",
         device="cuda",
         default_dtype="float32",
@@ -33,21 +31,11 @@ for temp in temperatures:
         gpu_preprocessing=False,
         calculator_type="mace_mp"
     )
-    result = widom_insertion.run(num_insertions=5000, random_seed=0, fold=1, grid_spacing=0.2)
+    result = widom_insertion.run(num_insertions=5000, random_seed=0, fold=2, grid_spacing=0.2)
     henry_coeff.append(np.mean(result["henry_coefficient"]))  
     pressure_data.append(np.mean(result["pressure"]))
     qst_data.append(np.mean(result["heat_of_adsorption"]))
     uptake_data.append(np.mean(result["uptake"]))
-
-# fixed_uptakes = np.linspace(0.1, 0.9, 10)
-
-df = pd.DataFrame({
-    "Uptake_(mmol/g)": uptake_data,
-    "Isosteric_Heat_of_Adsorption_(kJ/mol)": qst_data,
-    "Henry_Coefficient_(mol/kg_Pa)": henry_coeff,
-})
-output_file = "isosteric_heat_of_adsorption.csv"
-df.to_csv(output_file, index=False)
 
 qst_data = np.abs(np.array(qst_data))
 uptake_data = np.array(uptake_data)*1e-3
@@ -56,11 +44,9 @@ uptake_data = np.array(uptake_data)*1e-3
 plt.figure()
 plt.plot(uptake_data, qst_data, label="COF-999", marker="o")
 plt.ylim(0, 100)
-#plt.xlim(0, 1)
 plt.xlabel("Uptake (mmol/g)")
 plt.ylabel("Isosteric Heat of Adsorption (kJ/mol)")
-#plt.title("Isosteric Heat of CO2 Adsorption vs Uptake")
 plt.legend()
 plt.grid()
-plt.savefig("isosteric_heat_of_adsorption_vs_uptake_mace.png")
+plt.savefig("isosteric_heat_of_adsorption_vs_uptake.png")
 plt.show()
